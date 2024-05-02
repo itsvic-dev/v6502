298339dfb15eb6a6b58db3f6e0b80213293de366
Victor B. <contact@itsvic.dev>
2024-05-02 13:00:45 +0200

    memset for reproducibility

--- tests/cpuFixture.cpp
+++ tests/cpuFixture.cpp
@@ -1,7 +1,11 @@
 #include "tests/cpuFixture.h"
 #include <cstdint>
+#include <cstring>
 
-SimpleRAMBus::SimpleRAMBus() { ram = new uint8_t[0x10000]; }
+SimpleRAMBus::SimpleRAMBus() {
+  ram = new uint8_t[0x10000];
+  memset(ram, 0, 0x10000);
+}
 SimpleRAMBus::~SimpleRAMBus() { delete[] ram; }
 
 uint8_t SimpleRAMBus::read(uint16_t addr) { return ram[addr]; }
