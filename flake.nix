81f187a0c6265469a1fe0e7537db93620daa12e8
Victor B. <contact@itsvic.dev>
2024-05-04 16:50:55 +0200

    add default package to flake, add pkgconfig and install headers

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "v6502 - an experimental 6502 CPU emulator.";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -18,6 +18,18 @@
         pkgs = import nixpkgs { inherit system; };
       in
       {
+        packages.default = pkgs.stdenv.mkDerivation rec {
+          pname = "v6502";
+          version = "1.0.0";
+          src = ./.;
+          nativeBuildInputs = with pkgs; [
+            ninja
+            meson
+          ];
+          doCheck = true;
+          mesonFlags = [ "-Dtests=true" ];
+        };
+
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             meson
